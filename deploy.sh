e95cc79e997aada1624bae524be776331b8d10df
quliangen <quliangen@autohome.com.cn>
2026-03-05 07:22:06 +0800

    chore: add deploy script for gh-pages

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# 忽略错误
+set -e
+
+# 构建
+pnpm run build
+
+# 进入待发布的目录
+cd dist
+
+# 清理 .git 解决：Reinitialized existing Git repository
+find . -name ".git"
+rm -rf ./.git
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 部署到 https://quliangen.github.io/eight-planets/
+git push -f git@github.com:quliangen/eight-planets.git main:gh-pages
+
+cd -
